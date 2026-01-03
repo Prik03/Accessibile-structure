b1e7944870d0dcf58d4c2fe9258a8530dcb8b01f
Prik03 <princesain9319@gmail.com>
2026-01-04 00:32:06 +0530

    add blogs and fix main and aside on hover issue

--- script.js
+++ script.js
@@ -15,4 +15,38 @@ $('.showtags').on('mouseleave',function() {
     $(this).unwrap();   
 });
 
+const blogs = [
+  {
+    title: "Understanding Web Accessibility",
+    description: "Learn the basics of web accessibility and why it matters.",
+    link: "#"
+  },
+  {
+    title: "Top 10 Accessibility Tips for Developers",
+    description: "Practical tips to make your websites accessible.",
+    link: "#"
+  },
+  {
+    title: "How to Test Your Website for Accessibility",
+    description: "Tools and techniques to test accessibility effectively.",
+    link: "#"
+  }
+];
+
+const blogsList = $('#blogs-list');
+
+blogs.forEach((blog, index) => {
+  const blogItem = `
+    <li class="showtags">
+      <article aria-labelledby="blog-title-${index}" class="showtags">
+        <a href="${blog.link}" id="blog-title-${index}" class="showtags">
+          ${blog.title}
+        </a>
+        <p>${blog.description}</p>
+      </article>
+    </li>
+  `;
+  blogsList.append(blogItem);
+});
+
 });
